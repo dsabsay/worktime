4e2fed013a1ced30293b3e8d8c9aaeadd802935b
Daniel Sabsay <dsabsay@calpoly.edu>
2019-03-04 20:29:29 -0800

    Fix null reference

--- src/components/DataViewer.js
+++ src/components/DataViewer.js
@@ -2,7 +2,7 @@ import { ElementaryFunc } from '../../elementary.js';
 import { div } from '../../elementary.js';
 
 const DataViewer = ElementaryFunc((props) => {
-  const data = localStorage.getItem('records');
+  const data = localStorage.getItem('records') || 'hi';
 
   return div(
     data
